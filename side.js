f89f774f003181addf86ce383e86a542014bf0fc
Pavel Seidl <91326893+p4v32@users.noreply.github.com>
2023-02-01 17:17:05 +0100

    uploadind files

---
+++ side.js
@@ -0,0 +1,9 @@
+function dysplay() {
+  document.getElementById('content').style.marginLeft ='200px';
+  document.getElementById('side').hidden = false;
+}
+
+function hide(){
+  document.getElementById('content').style.marginLeft ='0px';
+  document.getElementById('side').hidden = true;
+}
---
+++ svitch.js
@@ -0,0 +1,69 @@
+function light1() {
+  document.getElementById('butondark').style.backgroundColor = 'black';
+  document.getElementById('butonlight').style.backgroundColor = 'var(--myorange)';
+  document.getElementById('butondark').style.borderWidth = '0px';
+  document.getElementById('butonlight').style.borderWidth = '2px';
+  document.getElementById('butonlight').style.borderRadius = '20px';
+  document.getElementById('butonlight').style.borderColor = 'var(--myorange2)';
+  //document.getElementById('butonlight').style.color = 'Black';
+  //document.getElementById('butondark').style.color = 'white';
+  document.body.style.color = 'Black';
+  document.body.style.backgroundColor = 'white';
+  document.body.style.setProperty("--codebg", "lightGray")
+  document.body.style.setProperty("--codeborder", "black")
+  document.body.style.setProperty("--linkcolor", "blue")
+}
+
+
+function dark1() {
+  document.getElementById('butondark').style.backgroundColor = 'var(--myorange)';
+  document.getElementById('butonlight').style.backgroundColor = 'Black';
+  document.getElementById('butondark').style.borderWidth = '2px';
+  document.getElementById('butonlight').style.borderWidth = '0px';
+  document.getElementById('butonlight').style.borderRadius = '20px';
+  //document.getElementById('butonlight').style.borderColor = 'Black';
+  //document.getElementById('butonlight').style.color = 'white';
+//  document.getElementById('butondark').style.color = 'Black';
+  document.body.style.backgroundColor = '#0b172a';
+  document.body.style.color = 'white';
+  document.body.style.setProperty("--codebg", "#07101e")
+  document.body.style.setProperty("--codeborder", "Cyan")
+  document.body.style.setProperty("--linkcolor", "Cyan")
+
+}
+
+
+function lighti() {
+  document.getElementById('butondark').style.backgroundColor = 'black';
+  document.getElementById('butonlight').style.backgroundColor = '#d65813';
+  document.getElementById('butondark').style.borderWidth = '0px';
+  document.getElementById('butonlight').style.borderWidth = '2px';
+  document.getElementById('butonlight').style.borderRadius = '20px';
+  document.getElementById('butonlight').style.borderColor = 'var(--myorange2)';
+  //document.getElementById('butonlight').style.color = 'Black';
+  //document.getElementById('butondark').style.color = 'white';
+  document.body.style.backgroundColor = 'white';
+  //document.getElementById('2s').style.backgroundColor = 'white';
+  //document.getElementById('2s').style.borderColor = 'black';
+  document.body.style.color = 'Black';
+  //document.getElementById('1s').style.borderColor = 'Black';
+  document.body.style.borderColor = 'Black';
+  document.body.table.td.style.borderColor = 'Cyan';
+
+}
+
+function darki() {
+  document.getElementById('butonlight').style.backgroundColor = 'black';
+  document.getElementById('butondark').style.backgroundColor = '#d65813';
+  document.getElementById('butonlight').style.borderWidth = '0px';
+  //document.getElementById('butondark').style.color = 'black';
+  //document.getElementById('butonlight').style.color = 'white';
+  document.getElementById('butondark').style.borderWidth = '2px';
+  document.body.style.backgroundColor = '#0b172a';
+  document.body.style.color = 'white';
+  //document.getElementById('1s').style.borderColor = 'Cyan';
+//  document.getElementById('2s').style.backgroundColor = '#0b172a';
+//  document.getElementById('2s').style.borderColor = 'Cyan';
+  document.body.style.borderColor = 'Cyan';
+
+}
